55af051e2385bd8f0131dad7dedd94aa27e3c776
bassiouny <ahmedbassiouny26@gmail.com>
2017-10-19 13:51:24 +0200

    add photo

--- app/src/main/java/com/example/ahmed/eyecare/activity/MenuContainer.java
+++ app/src/main/java/com/example/ahmed/eyecare/activity/MenuContainer.java
@@ -1,11 +1,10 @@
 package com.example.ahmed.eyecare.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.ahmed.eyecare.R;
 import com.example.ahmed.eyecare.fragment.MenuFragment;
-import com.example.ahmed.eyecare.fragment.SpeakerListFragment;
 import com.example.ahmed.eyecare.utils.Utils;
 
 public class MenuContainer extends AppCompatActivity {
@@ -14,11 +13,6 @@ public class MenuContainer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_container);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Utils.goToFragment(this,new MenuFragment(),null,null);
+        Utils.goToFragment(this, new MenuFragment(), null, null);
     }
 }
--- app/src/main/java/com/example/ahmed/eyecare/fragment/PhotosFragment.java
+++ app/src/main/java/com/example/ahmed/eyecare/fragment/PhotosFragment.java
@@ -1,6 +1,8 @@
 package com.example.ahmed.eyecare.fragment;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,10 +11,14 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ahmed.eyecare.R;
 import com.example.ahmed.eyecare.adapter.PhotoListAdapter;
@@ -20,7 +26,9 @@ import com.example.ahmed.eyecare.api.utils.RetrofitRequest;
 import com.example.ahmed.eyecare.api.utils.RetrofitResponse;
 import com.example.ahmed.eyecare.model.Photo;
 import com.example.ahmed.eyecare.utils.SharedPref;
+import com.mvc.imagepicker.ImagePicker;
 
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 /**
@@ -33,6 +41,7 @@ public class PhotosFragment extends Fragment {
     ProgressBar progress;
     private Toolbar mToolbar;
     private PhotoListAdapter photoListAdapter;
+    private TextView btnSelectImage;
 
 
     public PhotosFragment() {
@@ -44,13 +53,38 @@ public class PhotosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photos, container, false);
+        View view= inflater.inflate(R.layout.fragment_photos, container, false);
+        findViewById(view);
+        return view;
     }
 
+
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        findViewById(view);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        onClick();
+        loadData();
+    }
+
+    private void onClick() {
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.pickImage(PhotosFragment.this, "Select your image:");
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = ImagePicker.getImageFromResult(getContext(), requestCode, resultCode, data);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
     }
 
     private void findViewById(View view) {
@@ -59,6 +93,7 @@ public class PhotosFragment extends Fragment {
         recycleview = view.findViewById(R.id.recycleview);
         progress = view.findViewById(R.id.progress);
         ((AppCompatActivity) getActivity()).setSupportActionBar(mToolbar);
+        btnSelectImage = view.findViewById(R.id.btn_select_image);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
 
@@ -69,13 +104,12 @@ public class PhotosFragment extends Fragment {
                 getActivity().onBackPressed();
             }
         });
-        recycleview.setLayoutManager(new GridLayoutManager(getActivity(), GridLayoutManager.DEFAULT_SPAN_COUNT));
+        recycleview.setLayoutManager(new GridLayoutManager(getActivity(), 3));
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        loadData();
     }
 
     private void loadData() {
@@ -84,11 +118,13 @@ public class PhotosFragment extends Fragment {
             public void onSuccess(List<Photo> photos) {
                 photoListAdapter=new PhotoListAdapter(getContext(),photos);
                 recycleview.setAdapter(photoListAdapter);
+                progress.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailed(String errorMessage) {
-
+                Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
+                progress.setVisibility(View.GONE);
             }
         });
     }
